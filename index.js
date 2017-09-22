5df8bab15a7fce1c91592e784490ba7c189df960
goddy <onememeonly@gmail.com>
2017-09-22 07:40:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ let args = msg.content.split(" ").slice(1);
   if (msg.content.startsWith(prefix + "say")) {
   let args = msg.content.split(" ").slice(1).join(" ")
   msg.delete()
-  if(msg.author.id !== "260246864979296256") return;
+  if(msg.author.id !== "260246864979296256" || "285336677390745600") return;
   if (!args) {
     return msg.channel.send("Whoops, no args were involved. Try again")
   }
